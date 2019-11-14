a76dfaf0eb2e967884581f047c9f7d85411eff3e
David P. Sanders <dpsanders@gmail.com>
2019-11-14 16:34:34 -0500

    Files generated by PkgTemplates

---
+++ src/HardSphereDynamics.jl
@@ -0,0 +1,5 @@
+module HardSphereDynamics
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using HardSphereDynamics
+using Test
+
+@testset "HardSphereDynamics.jl" begin
+    # Write your own tests here.
+end
